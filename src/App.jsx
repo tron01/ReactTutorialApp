6089e09e3581d6047ef5c9842775e2f6f7c11468
tron01 <69385258+tron01@users.noreply.github.com>
2025-05-12 22:14:05 +0530

    fix useStates with Dict

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 
@@ -6,27 +6,32 @@ import './App.css'
 function App() {
 
   const Data = {
-      kerala:"kochi",
-      goa:"goa",
-      tvm:"tvm"
+      kerala:"Tvm",
+      Goa:"goa",
+      TamilNadu:"Chennai"
   }
 
-    const [isSelect, setIsSelect] = useState("kochi")
+    const [isSelect, setIsSelect] = useState("kerala")
+    const [selectedCaptial, setSelectedCaptial] = useState("Tvm")
     
     const handleChange = (e) =>{
       console.log(e.target.value);
       setIsSelect(e.target.value)
+      setSelectedCaptial((Data[e.target.value]))
     };
-    console.log("selected:"+isSelect);
+    //console.log("selected:"+isSelect);
     
   return (
     <>
             <h1>Hello App Componet</h1>
             <select onChange={handleChange}>
-            <option value="kochi">"afaffafa"</option>
-            <option value="kerala2">"afaffafaafafa"</option>
+            <option value="kerala">Kerala</option>
+            <option value="Goa">Goa</option>
+            <option value="TamilNadu">TamilNadu</option>
             </select>
             <h2> Selected : {isSelect}</h2>
+            <h2> Captial :{selectedCaptial} </h2>
+            <p> </p>
   
     </>
   );
