2d45eb3719c1a539e795bd30e0dfcd2d8c2beabf
tron01 <69385258+tron01@users.noreply.github.com>
2025-05-12 22:01:27 +0530

    useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,33 @@
 import { useState } from 'react'
 import './App.css'
-import Card from './componets/Card';
+
 
 
 function App() {
 
-  const cardInfoList = [
-    { title: 'Card 1', description: 'This is card 1' },
-    { title: 'Card 2', description: 'This is card 2' },
-    { title: 'Card 3', description: 'This is card 3' },
-  ];
+  const Data = {
+      kerala:"kochi",
+      goa:"goa",
+      tvm:"tvm"
+  }
 
+    const [isSelect, setIsSelect] = useState("kochi")
+    
+    const handleChange = (e) =>{
+      console.log(e.target.value);
+      setIsSelect(e.target.value)
+    };
+    console.log("selected:"+isSelect);
+    
   return (
     <>
             <h1>Hello App Componet</h1>
-
-            {cardInfoList.map((c)=>
-            (<Card desc={c.description} title={c.title}  ></Card>
-            ))}
+            <select onChange={handleChange}>
+            <option value="kochi">"afaffafa"</option>
+            <option value="kerala2">"afaffafaafafa"</option>
+            </select>
+            <h2> Selected : {isSelect}</h2>
+  
     </>
   );
 }
