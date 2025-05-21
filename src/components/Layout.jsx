b960e968aa50d8c697d2991ea7601a2b93bca9aa
tron01 <69385258+tron01@users.noreply.github.com>
2025-05-22 00:05:04 +0530

    Layout

---
+++ src/components/Layout.jsx
@@ -0,0 +1,14 @@
+import AppNavbar from './Navbar';
+import Footer from './Footer';
+
+const Layout = ({ children }) => (
+  <div className="d-flex flex-column min-vh-100">
+    <AppNavbar />
+      <main className="flex-grow-1 py-4">
+        {children}
+      </main>
+    <Footer />
+  </div>
+);
+
+export default Layout;
