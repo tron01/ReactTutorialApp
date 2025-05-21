91ea876dcc4addd39dd346f97398f54180db2819
tron01 <69385258+tron01@users.noreply.github.com>
2025-05-22 00:05:13 +0530

    Home  page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import { Container } from 'react-bootstrap';
+
+const Home = () => (
+  <Container>
+    <h1>Welcome to MyApp</h1>
+    <p>This is the homepage content styled with Bootstrap.</p>
+  </Container>
+);
+
+export default Home;
