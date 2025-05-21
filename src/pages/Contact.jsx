f44716e907e5d4edde295363be69739e5ecb5a7e
tron01 <69385258+tron01@users.noreply.github.com>
2025-05-22 00:47:43 +0530

    new pages

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+const Contact = () => (
+  <Container>
+    <h1>Contact</h1>
+    <p>This is the contact page.</p>
+  </Container>
+);
+
+export default Contact;
+
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+const NotFound = () => (
+  <Container className="text-center">
+    <h1 className="display-4">404</h1>
+    <p className="lead">Page Not Found</p>
+  </Container>
+);
+
+export default NotFound;
