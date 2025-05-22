0b238454ca44abf32f0b9f3deafd36cc7e261084
tron01 <69385258+tron01@users.noreply.github.com>
2025-05-22 15:35:18 +0530

    Theme context and toggle setup

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,32 @@
-import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Navbar, Nav, Container, Button } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
+import { useTheme } from '../context/ThemeContext';
 
-const AppNavbar = () => (
-  <Navbar bg="dark" variant="dark" expand="lg">
-    <Container>
-      <Navbar.Brand as={NavLink} to="/">MyApp</Navbar.Brand>
-      <Navbar.Toggle aria-controls="main-navbar" />
-      <Navbar.Collapse id="main-navbar">
-        <Nav className="ms-auto">
-          <Nav.Link as={NavLink} to="/" end>Home</Nav.Link>
-          <Nav.Link as={NavLink} to="/about">About</Nav.Link>
-          <Nav.Link as={NavLink} to="/contact">Contact</Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
-    </Container>
-  </Navbar>
-);
+const AppNavbar = () => {
+  const { theme, toggleTheme } = useTheme();
+
+  return (
+    <Navbar expand="lg">
+      <Container>
+        <Navbar.Brand as={NavLink} to="/">MyApp</Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse>
+          <Nav className="ms-auto">
+            <Nav.Link as={NavLink} to="/" end>Home</Nav.Link>
+            <Nav.Link as={NavLink} to="/about">About</Nav.Link>
+            <Nav.Link as={NavLink} to="/contact">Contact</Nav.Link>
+            <Button
+              onClick={toggleTheme}
+              variant={theme === 'dark' ? 'light' : 'dark'}
+              className="ms-2"
+            >
+              {theme === 'dark' ? 'Light Mode' : 'Dark Mode'}
+            </Button>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
 
 export default AppNavbar;
--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,16 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { ThemeProvider } from './context/ThemeContext';
+import './theme.css';
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+    <ThemeProvider>
+        <App />
+      </ThemeProvider>
     </BrowserRouter>
   </StrictMode>
 );
